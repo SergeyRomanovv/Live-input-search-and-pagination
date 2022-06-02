2dad39d07d5212efa1f2977410162f567e9072a5
Sergey Romanov <study-front@mail.ru>
2022-06-02 11:46:13 +0300

    initial project

---
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SearchForm() {
+  return (
+    <>
+      <div className='header'>
+        <p>Сотрудники</p>
+      </div>
+      
+    </>
+  )
+}
